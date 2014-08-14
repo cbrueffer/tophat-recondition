1c9bb04c8f40ebc9207d1875384bfcc21cecda06
Christian Brueffer <christian@brueffer.de>
2014-08-14 15:39:25 +0200

    Use the "with" statement to make the code more clear.

--- fix_tophat_unmapped_reads.py
+++ fix_tophat_unmapped_reads.py
@@ -26,65 +26,58 @@ def get_index_pos(index, unmapped_reads, read):
 
 
 def main(path, outdir, mapped_file="accepted_hits.bam", unmapped_file="unmapped.bam"):
-    bam_mapped = pysam.Samfile(os.path.join(path, mapped_file))
-    bam_unmapped = pysam.Samfile(os.path.join(path, unmapped_file))
-    unmapped_reads = list(bam_unmapped.fetch(until_eof=True))
-
     # Fix things that relate to all unmapped reads.
     unmapped_dict = {}
     unmapped_index = {}
-    for i in range(len(unmapped_reads)):
-        read = unmapped_reads[i]
+    with pysam.Samfile(os.path.join(path, unmapped_file)) as bam_unmapped:  
+        unmapped_reads = list(bam_unmapped.fetch(until_eof=True))
+        for i in range(len(unmapped_reads)):
+            read = unmapped_reads[i]
 
-        # remove /1 and /2 suffixes
-        if read.qname.find("/") != -1:
-            read.qname = read.qname[:-2]
+            # remove /1 and /2 suffixes
+            if read.qname.find("/") != -1:
+                read.qname = read.qname[:-2]
 
-        unmapped_index[read.qname] = i
+            unmapped_index[read.qname] = i
 
-        # work around "mate is unmapped" bug in TopHat
-        if read.qname in unmapped_dict:
-            unmapped_reads[unmapped_dict[read.qname]].mate_is_unmapped = True
-            read.mate_is_unmapped = True
-        else:
-            unmapped_dict[read.qname] = i
+            # work around "mate is unmapped" bug in TopHat
+            if read.qname in unmapped_dict:
+                unmapped_reads[unmapped_dict[read.qname]].mate_is_unmapped = True
+                read.mate_is_unmapped = True
+            else:
+                unmapped_dict[read.qname] = i
 
-        read.mapq = 0
+            read.mapq = 0
 
-        unmapped_reads[i] = read
+            unmapped_reads[i] = read
 
-    # Fix things that relate only to unmapped reads with a mapped mate.
-    for mapped in bam_mapped:
-        if mapped.mate_is_unmapped:
-            i = get_index_pos(unmapped_index, unmapped_reads, mapped)
-            if i is not None:
-                unmapped = unmapped_reads[i]
+        # Fix things that relate only to unmapped reads with a mapped mate.
+        with pysam.Samfile(os.path.join(path, mapped_file)) as bam_mapped:
+            for mapped in bam_mapped:
+                if mapped.mate_is_unmapped:
+                    i = get_index_pos(unmapped_index, unmapped_reads, mapped)
+                    if i is not None:
+                        unmapped = unmapped_reads[i]
 
-                # map chromosome TIDs from mapped to unmapped file
-                mapped_rname = bam_mapped.getrname(mapped.tid)
-                unmapped_new_tid = bam_unmapped.gettid(mapped_rname)
+                        # map chromosome TIDs from mapped to unmapped file
+                        mapped_rname = bam_mapped.getrname(mapped.tid)
+                        unmapped_new_tid = bam_unmapped.gettid(mapped_rname)
 
-                unmapped.tid = unmapped_new_tid
-                unmapped.rnext = unmapped_new_tid
-                unmapped.pos = mapped.pos
-                unmapped.pnext = 0
+                        unmapped.tid = unmapped_new_tid
+                        unmapped.rnext = unmapped_new_tid
+                        unmapped.pos = mapped.pos
+                        unmapped.pnext = 0
 
-                unmapped_reads[i] = unmapped
+                        unmapped_reads[i] = unmapped
 
-    bam_mapped.close()
+        # for the output file, take the headers from the unmapped file
+        base, ext = os.path.splitext(unmapped_file)
+        out_filename = "".join([base, "_fixup", ext])
 
-    # for the output file, take the headers from the unmapped file
-    base, ext = os.path.splitext(unmapped_file)
-    out_filename = "".join([base, "_fixup", ext])
-    bam_out = pysam.Samfile(os.path.join(outdir, out_filename), "wb",
-                        template=bam_unmapped)
-
-    bam_unmapped.close()
-
-    for read in unmapped_reads:
-        bam_out.write(read)
-
-    bam_out.close()
+        with pysam.Samfile(os.path.join(outdir, out_filename), "wb",
+                           template=bam_unmapped) as bam_out:
+            for read in unmapped_reads:
+                bam_out.write(read)
 
 
 def usage(scriptname):
