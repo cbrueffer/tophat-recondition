c3df99bd38986800d0acb50d4e903087265ad445
Christian Brueffer <christian@brueffer.de>
2015-02-19 13:24:31 +0100

    Update copyright.

--- tophat-recondition.py
+++ tophat-recondition.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 #
-# Copyright (c) 2013-2014 Christian Brueffer (ORCID: 0000-0002-3826-0989)
+# Copyright (c) 2013-2015 Christian Brueffer (ORCID: 0000-0002-3826-0989)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
